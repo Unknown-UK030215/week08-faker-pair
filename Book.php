5c91bd950071c15309f6673bfd8ad54fcdae5ad7
Unknown-UK030215 <152607455+Unknown-UK030215@users.noreply.github.com>
2025-02-27 23:22:40 +0800

    Fix an error

--- Book.php
+++ Book.php
@@ -46,16 +46,6 @@
     tbody tr td {
         text-align: center;
         font-family: 'Times New Roman', Times, serif;
-        background-color: aqua;
-    }
-
-    .container-md{
-        background-color: #ffffff; /* White container background */
-        padding: 20px;
-        border-radius: 10px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Soft shadow effect */
-        margin: auto;
-        width: 80%;
     }
 </style>
 <body>
