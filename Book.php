6e68b4175e879c4df379314a099fe6a2125bf338
nanazo08 <gianajuanzo@gmail.com>
2025-02-27 10:02:01 +0800

    add some styles

--- Book.php
+++ Book.php
@@ -3,22 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Books</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
+<style>
+    h1{
+        text-align: center;
+        font-family: 'Times New Roman', Times, serif;
+    }
+</style>
 <body>
-<h2>Books</h2>
-    <table>
-        <thead>
-            <tr>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Publication Year</th>
-                <th>ISBN</th>
-                <th>Genre</th>
-                <th>Summary</th>
-            </tr>
-        </thead>
-    
+    <div>
+        <h1>Books</h1>
+        <table class="table table-bordered table-striped">
+            <thead class="table-dark">
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Publication Year</th>
+                    <th>ISBN</th>
+                    <th>Genre</th>
+                    <th>Summary</th>
+                </tr>
+            </thead>
+    </div>
 </body>
 </html>
