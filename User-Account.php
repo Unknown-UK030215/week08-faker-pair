9824da27a9884094e72aeb57e9584848f47419f1
Unknown-UK030215 <santosmarkvincent15@gmail.com>
2025-02-27 23:24:21 +0800

    Modified PHP

--- User-Account.php
+++ User-Account.php
@@ -5,6 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fake User Account</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <style>
+        h2 {
+            text-align: center;
+            font-family: 'Times New Roman', Times, serif;
+            font-size: 15;
+            font-weight: bolder;
+        }
+        div table thead tr th {
+            font-family: 'Times New Roman', Times, serif;
+            text-align: center;
+        }
+        body {
+            background-color: lightgray;
+        }
+    </style>
 </head>
 <body>
     <div class=".container-md">
@@ -21,7 +36,29 @@
                 </tr>
             </thead>
             <tbody>
-                
+                <?php
+                    require 'vendor/autoload.php';
+
+                    $faker = Faker\Factory::create('en_PH');
+
+                    for ($i = 0; $i < 10; $i++) {
+                        $fullName = $faker->name;
+                        $email = $faker->email;
+                        $username = explode('@', $email)[0];
+                        $password = hash('sha256', $faker->password);
+                        $uuid = $faker->uuid;
+                        $createdAt = $faker->dateTimeBetween('-2 years', 'now')->format('Y-m-d H:i:s');
+
+                        echo "<tr>";
+                        echo "<td>$uuid</td>";
+                        echo "<td>$fullName</td>";
+                        echo "<td>$email</td>";
+                        echo "<td>$username</td>";
+                        echo "<td class='password-cell'>$password</td>";
+                        echo "<td>$createdAt</td>";
+                        echo "</tr>";
+                    }
+                ?>
             </tbody>
         </table>
     </div>
